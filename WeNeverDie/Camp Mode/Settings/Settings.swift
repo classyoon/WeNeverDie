8fd705d6e7e42e041cbaef00e3f77c25cff93ad4
Conner Yoon <con.yoon@icloud.com>
2023-03-21 23:12:52 -0400

    Fixed sound button.

--- WeNeverDie/Camp Mode/Settings/Settings.swift
+++ WeNeverDie/Camp Mode/Settings/Settings.swift
@@ -11,35 +11,32 @@ struct Settings: View {
     @ObservedObject var gameData: ResourcePool
     
     @State var musicIsPlaying = false
-    var muteButton : some View {
-        Button {
-            if musicIsPlaying {
-                guard (musicPlayer?.pause()) != nil else {
-                    fatalError("Unable to pause music")
-                }
-                musicIsPlaying = musicPlayer?.isPlaying == true
-            } else {
-                guard (musicPlayer?.play()) != nil else {
-                    fatalError("Unable to start music")
-                }
-                musicIsPlaying = musicPlayer?.isPlaying == true
-            }
-        } label: {
-            Image(systemName: musicIsPlaying ? "speaker.wave.2.circle.fill" : "speaker.slash.circle")
-                .resizable()
-                .aspectRatio(1, contentMode: .fit)
-                .foregroundColor(musicIsPlaying ? Color(.green) : Color(.red).opacity(0.5))
-                .shadow(radius: 5)
-        }.frame(maxHeight: 50)
-    }
     var body: some View {
         VStack (alignment: gameData.switchToLeft ? .leading : .trailing){
-            //            Text(devMode ? "Game Developer Mode on (go back to camp to update)":"Game Developer Mode off (go back to camp to update)").foregroundColor(Color.blue)
-            //            Button("Developer Mode Toggle") {
-            //                devMode.toggle()
-            //                print("devMode \(devMode)")
-            //            }.buttonStyle(.bordered)
-            muteButton
+//                        Text(devMode ? "Game Developer Mode on (go back to camp to update)":"Game Developer Mode off (go back to camp to update)").foregroundColor(Color.blue)
+//                        Button("Developer Mode Toggle") {
+//                            devMode.toggle()
+//                            print("devMode \(devMode)")
+//                        }.buttonStyle(.bordered)
+            Button {
+                if musicIsPlaying {
+                    guard (musicPlayer?.pause()) != nil else {
+                        fatalError("Unable to pause music")
+                    }
+                    musicIsPlaying = musicPlayer?.isPlaying == true
+                } else {
+                    guard (musicPlayer?.play()) != nil else {
+                        fatalError("Unable to start music")
+                    }
+                    musicIsPlaying = musicPlayer?.isPlaying == true
+                }
+            } label: {
+                Image(systemName: musicIsPlaying ? "speaker.wave.2.circle.fill" : "speaker.slash.circle")
+                    .resizable()
+                    .aspectRatio(1, contentMode: .fit)
+                    .foregroundColor(musicIsPlaying ? Color(.green) : Color(.red).opacity(0.5))
+                    .shadow(radius: 5)
+            }.frame(maxHeight: 50)
             Button(gameData.switchToLeft ? "Left Hand Mode On" : "Left Hand Mode Off"){
                 gameData.toggleLeftHandMode()
             }.buttonStyle(.bordered)
