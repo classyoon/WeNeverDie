ee3de48442db10ba5158929c1c9fee071bfe29f3
Conner Yoon <con.yoon@icloud.com>
2023-04-14 18:41:18 -0400

    Minor edits to zombie.

--- WeNeverDie/Outside Mode/Mob Functions/ZombieFunc.swift
+++ WeNeverDie/Outside Mode/Mob Functions/ZombieFunc.swift
@@ -13,7 +13,7 @@ extension Board {
     ///   - zombie: Seeker. Is an inout because alert is triggered here
     ///   - targetList: List of valid targets
     /// - Returns: The distance in row and column, and the location of the seeker
-    func findDistance(zombie : any Piece, targetList: [Coord])->(RowD : Int, ColD : Int, seekerCoord : Coord){//This properly locates the targets.
+    func percieveDistances(zombie : any Piece, targetList: [Coord])->(RowD : Int, ColD : Int, seekerCoord : Coord){//This properly locates the targets.
         var targetLoc = Coord(); let seekerLoc = getCoord(of: zombie) ?? Coord()
         var thingSighted = false
         var DRow : Int = 100; var DCol : Int = 100
@@ -34,7 +34,7 @@ extension Board {
     }
     
 
-    func attemptToAttack(selfPiece : any Piece, selfLoc : Coord) ->Bool{
+    func attackNearbyPiece(selfPiece : any Piece, selfLoc : Coord) ->Bool{
         for r in safeNum(r: selfLoc.row-1)...safeNum(r: selfLoc.row+1) {//Checks if can attack
             for c in safeNum(c: selfLoc.col-1)...safeNum(c: selfLoc.col+1) {
                 if let nearbyPiece = board[r][c]{
@@ -59,7 +59,7 @@ extension Board {
     ///   - selfLoc: This is the piece's location on the board.
     ///   - zombie: This is the piece, allowing us to modify whether the piece is alert or not
     /// - Returns: This returns a coord for the piece to possibly go to and it returns whether the piece is chasing or not.
-    func returnChaseDirection(_ RowD : Int, _ ColD : Int, selfLoc : Coord, zombie :  any Piece) -> (target : Coord, isChasing: Bool){
+    func calculateChaseInfo(_ RowD : Int, _ ColD : Int, selfLoc : Coord, zombie :  any Piece) -> (pursuitCoord : Coord, isChasing: Bool){
         var directionText = "" //Allows for debugging printing
         var targetLoc = selfLoc
         if RowD > 0 {
@@ -75,6 +75,7 @@ extension Board {
             targetLoc.col+=1; directionText+="Right"
         }
         if !willCollide(zombie, targetLoc) {//Check if will collide
+            print("I am moving \(directionText) to chase a target")
             return (targetLoc, true)
         }
         else{
@@ -90,8 +91,7 @@ extension Board {
         }
         return true
     }
-    
-    func wander(_ zombie : any Piece, _ selfCoord : Coord) -> Coord{
+    func provideWanderingCoord(_ zombie : any Piece, _ selfCoord : Coord) -> Coord{
         //Wandering
         let ranRow = safeNum(r: selfCoord.row+Int.random(in: -1...1))
         let ranCol = safeNum(c: selfCoord.col+Int.random(in: -1...1))
@@ -102,7 +102,7 @@ extension Board {
             return selfCoord
         }
     }
-    func moveZombies(_ zombies : [any Piece], playerCoords: [Coord], zombieLoc: [Coord]){//Collects list of zombie
+    func runBehaviorOfAllZombies(_ zombies : [any Piece], playerCoords: [Coord], zombieLoc: [Coord]){//Collects list of zombie
         var zombies = zombies.self
         for currentZombie in 0..<zombies.count {
             executeZombieBehavior(zombie: &zombies[currentZombie], targetList: playerCoords)
@@ -110,24 +110,24 @@ extension Board {
         }
     }
     func executeZombieBehavior(zombie : inout any Piece, targetList: [Coord]){
-        let distance = findDistance(zombie: zombie, targetList: targetList)
-        let chaseStatus = returnChaseDirection(distance.RowD, distance.ColD, selfLoc: distance.seekerCoord, zombie: zombie)
+        let percievedDistances = percieveDistances(zombie: zombie, targetList: targetList)
+        let chaseInformation = calculateChaseInfo(percievedDistances.RowD, percievedDistances.ColD, selfLoc: percievedDistances.seekerCoord, zombie: zombie)
         
-        if attemptToAttack(selfPiece: zombie, selfLoc: distance.seekerCoord) {
+        if attackNearbyPiece(selfPiece: zombie, selfLoc: percievedDistances.seekerCoord) {
             print("Attack")
             zombie.alert = true
-            board[distance.seekerCoord.row][distance.seekerCoord.col] = zombie
+            board[percievedDistances.seekerCoord.row][percievedDistances.seekerCoord.col] = zombie
         }
-        else if chaseStatus.isChasing{
+        else if chaseInformation.isChasing{
             print("Chase")
             zombie.alert = true
-            move(&zombie, from: distance.seekerCoord, to: chaseStatus.target)
+            move(&zombie, from: percievedDistances.seekerCoord, to: chaseInformation.pursuitCoord)
         }
         else{
             print("Wander")
             zombie.alert = false
-            board[distance.seekerCoord.row][distance.seekerCoord.col] = zombie
-            move(&zombie, from: distance.seekerCoord, to: wander(zombie, distance.seekerCoord))
+            board[percievedDistances.seekerCoord.row][percievedDistances.seekerCoord.col] = zombie
+            move(&zombie, from: percievedDistances.seekerCoord, to: provideWanderingCoord(zombie, percievedDistances.seekerCoord))
          
         }
     }
