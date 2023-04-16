66a0a5f05194c9be8f3ab137ca0de579ffb1e02c
Conner Yoon <con.yoon@icloud.com>
2023-04-15 22:13:29 -0400

    Gigantic improvements in the readability of the player functions.

--- WeNeverDie/Outside Mode/Mob Functions/PlayerFunc.swift
+++ WeNeverDie/Outside Mode/Mob Functions/PlayerFunc.swift
@@ -11,89 +11,34 @@ extension Board {
     /// This reacts to each tap on the screen.
     ///
     // This function handles user input when a player taps on a tile on the game board.
-    func handleTap(tapRow: Int, tapCol: Int) {
-        
-        // Check if any unit is currently selected
-        if unitWasSelected == false {
-            // If no unit is selected, check if the tapped tile contains a player unit that can move
-            if board[tapRow][tapCol]?.getCanMove() == true && (board[tapRow][tapCol]?.isPlayerUnit==true) {
-                // If a player unit can move and is present on the tapped tile, select the unit and highlight the tile as blue
-                SelectUnitOn(tapRow, tapCol)
-            }
-            else if highlightSquare == Coord(tapRow, tapCol) {
-                highlightSquare = nil
-            }
-            else {
-                highlightSquare = Coord(tapRow, tapCol)//Select Tile
-                
-            }
-        }
-        else if var piece = selectedUnit, let startPoint = highlightSquare {
-            /**
-             If the second tap is on on a second piece
-             */
-            if let secondPiece = board[tapRow][tapCol]{
-                if secondPiece.isAttackable && isPossibleLoc(row: tapRow, col: tapCol) {
-                    board[tapRow][tapCol]?.health -= piece.damage
-                    board[startPoint.row][startPoint.col]?.incrementMoveCounter()
-                    deselectUnit()
-                    playerSoundPlayer?.prepareToPlay()
-                    playerSoundPlayer?.play()
-                    turn = UUID()
-                    
-                    
-                    if board[tapRow][tapCol]!.health <= 0 {
-                        board[tapRow][tapCol] = nil
-                    }
-                }
-                else if secondPiece.isRecruitable {
-                    board[tapRow][tapCol]?.trust+=piece.damage
-                    
-                    deselectUnit()
-                    board[startPoint.row][startPoint.col]?.incrementMoveCounter()
-                    turn = UUID()
-                    if secondPiece.trust >= 5 {
-                        set(moveable: playerUnit(name: "Jones", board: self), Coord: Coord(tapRow, tapCol))
-                        UnitsRecruited+=1
-                        turn = UUID()
-                        
-                    }
-                }
-                else if secondPiece.getCanMove() && secondPiece.isPlayerUnit {
-                    if highlightSquare == Coord(tapRow, tapCol) {
-                        // If the same tile is tapped twice, deselect the unit
-                        deselectUnit()
-                    }
-                    else {
-                        // If another player unit is tapped, switch the selected unit to the new unit
-                        SelectUnitOn(tapRow, tapCol)
-                    }
-                }
-                
-            }
-            else if isPossibleLoc(row: tapRow, col: tapCol) {
-                // If an empty tile is tapped and it is a possible location for the selected unit to move, move the unit to the new tile
-                playerwalkSoundPlayer?.prepareToPlay()
-                move(&piece, from: startPoint, to: Coord(row: tapRow, col: tapCol))
-                playerwalkSoundPlayer?.play()
-            }
-            else {
-                // If the tapped tile is not a possible location for the selected unit to move, deselect the unit
-                deselectUnit()
-            }
+    func handleAttackWithUnit(secondPiece : any Piece, piece : any Piece, tapRow : Int, tapCol : Int, startPoint : Coord){
+        board[tapRow][tapCol]?.health -= piece.damage
+        board[startPoint.row][startPoint.col]?.incrementMoveCounter()
+        deselectUnit()
+        playerSoundPlayer?.prepareToPlay()
+        playerSoundPlayer?.play()
+        turn = UUID()
+        if board[tapRow][tapCol]!.health <= 0 {
+            board[tapRow][tapCol] = nil
         }
     }
-    
-    func SelectUnitOn(_ tapRow: Int, _ tapCol: Int){
-        selectedUnit = board[tapRow][tapCol]
-        highlightSquare = Coord(tapRow, tapCol)
-    }
-    
-    func deselectUnit(){
-        selectedUnit = nil
-        highlightSquare = nil
-        canAnyoneMove = isAnyoneStillActive()
+    func handleRecruitWithUnit(secondPiece : any Piece, piece : any Piece, tapRow : Int, tapCol : Int, startPoint : Coord){
+        board[tapRow][tapCol]?.trust+=piece.damage
         
+        deselectUnit()
+        board[startPoint.row][startPoint.col]?.incrementMoveCounter()
+        turn = UUID()
+        if secondPiece.trust >= 5 {
+            set(moveable: playerUnit(name: "Jones", board: self), Coord: Coord(tapRow, tapCol))
+            UnitsRecruited+=1
+            turn = UUID()
+            
+        }
+    }
+    func movePlayerUnit(tapRow: Int, tapCol: Int, startPoint : Coord, piece : inout any Piece){
+        playerwalkSoundPlayer?.prepareToPlay()
+        move(&piece, from: startPoint, to: Coord(row: tapRow, col: tapCol))
+        playerwalkSoundPlayer?.play()
     }
     func isAnyoneStillActive()->Bool{
         let playerList = createLists().playerCoords
@@ -121,36 +66,29 @@ extension Board {
         }
     }
     func searchLocationVM(){
-        if unitWasSelected{
-            
-            
-            if var selected = selectedUnit {
+        if unitWasSelected, var selected = selectedUnit, let piece = getCoord(of: selected){ //?? nil
+            if selected.getCanMove(){
+                board[piece.row][piece.col]?.movementCount+=1//Upfront stamina cost.
+                selected.movementCount+=1
                 
-                if let piece = getCoord(of: selected){ //?? nil
-                    
-                    if selected.getCanMove(){
-                        board[piece.row][piece.col]?.movementCount+=1//Upfront stamina cost.
-                        selected.movementCount+=1
-                        if terrainBoard[piece.row][piece.col].loot>0{
-                            foodNew+=1
-                            terrainBoard[piece.row][piece.col].loot-=1
-                            grabSoundPlayer?.prepareToPlay()
-                            grabSoundPlayer?.play()
-                        }
-                        else {
-                            emptySoundPlayer?.prepareToPlay()
-                            emptySoundPlayer?.play()
-                        }
-                    }
-                    selectedUnit = selected
+                if terrainBoard[piece.row][piece.col].loot>0{
+                    foodNew+=1
+                    terrainBoard[piece.row][piece.col].loot-=1
+                    grabSoundPlayer?.prepareToPlay()
+                    grabSoundPlayer?.play()
+                }
+                else {
+                    emptySoundPlayer?.prepareToPlay()
+                    emptySoundPlayer?.play()
                 }
             }
-            
-            canAnyoneMove = isAnyoneStillActive()
-            turn = UUID()
-            if !(selectedUnit?.getCanMove() ?? true) {
-                deselectUnit()
-            }
+            selectedUnit = selected
+        }
+        canAnyoneMove = isAnyoneStillActive()
+        turn = UUID()
+        if !(selectedUnit?.getCanMove() ?? true) {
+            deselectUnit()
         }
     }
 }
+
---
+++ WeNeverDie/Outside Mode/Mob Functions/handleTapFunctions.swift
@@ -0,0 +1,72 @@
+//
+//  handleTapFunctions.swift
+//  WeNeverDie
+//
+//  Created by Conner Yoon on 4/15/23.
+//
+
+import Foundation
+extension Board {
+    func handleTap(tapRow: Int, tapCol: Int) {
+        !unitWasSelected ? handleTapWithoutSelectedUnit(tapRow: tapRow, tapCol: tapCol) : handleTapWithSelectedPiece(tapRow: tapRow, tapCol: tapCol)
+    }
+    
+    func handleTapWithSelectedPiece(tapRow: Int, tapCol: Int) {
+        if var piece = selectedUnit, let startPoint = highlightSquare {
+            if board[tapRow][tapCol] != nil{
+                handleTapOnSecondPiece(tapRow: tapRow, tapCol: tapCol, startPoint : startPoint)
+            }
+            else if isPossibleLoc(row: tapRow, col: tapCol) {
+                movePlayerUnit(tapRow: tapRow, tapCol: tapCol, startPoint: startPoint, piece: &piece)
+            }
+            else {
+                deselectUnit()
+            }
+        }
+    }
+    
+    func handleTapOnSecondPiece(tapRow: Int, tapCol : Int, startPoint : Coord){
+        if let piece = selectedUnit, let startPoint = highlightSquare, let secondPiece = board[tapRow][tapCol]{
+            if secondPiece.isAttackable && isPossibleLoc(row: tapRow, col: tapCol) {
+                handleAttackWithUnit(secondPiece: secondPiece, piece: piece, tapRow: tapRow, tapCol: tapCol, startPoint: startPoint)
+            }
+            else if secondPiece.isRecruitable {
+                handleRecruitWithUnit(secondPiece: secondPiece, piece: piece, tapRow: tapRow, tapCol: tapCol, startPoint: startPoint)
+            }
+            else if secondPiece.getCanMove() && secondPiece.isPlayerUnit {
+                handleSwitchingUnit(tapRow: tapRow, tapCol: tapCol)
+            }
+            
+        }
+    }
+    func handleTapWithoutSelectedUnit(tapRow: Int, tapCol: Int) {
+        if board[tapRow][tapCol]?.getCanMove() == true && (board[tapRow][tapCol]?.isPlayerUnit==true) {
+            SelectUnitOn(tapRow, tapCol)
+        }
+        else if highlightSquare == Coord(tapRow, tapCol) {
+            highlightSquare = nil
+        }
+        else {
+            highlightSquare = Coord(tapRow, tapCol)
+        }
+    }
+    func handleSwitchingUnit(tapRow: Int, tapCol : Int){
+        if highlightSquare == Coord(tapRow, tapCol) {
+            deselectUnit()
+        }
+        else {
+            SelectUnitOn(tapRow, tapCol)
+        }
+    }
+    
+    func SelectUnitOn(_ tapRow: Int, _ tapCol: Int){
+        selectedUnit = board[tapRow][tapCol]
+        highlightSquare = Coord(tapRow, tapCol)
+    }
+    
+    func deselectUnit(){
+        selectedUnit = nil
+        highlightSquare = nil
+        canAnyoneMove = isAnyoneStillActive()
+    }
+}
